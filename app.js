a22eb506549ebce082d2b16fbd9f76c2dcbbaf20
EC2 Default User <ec2-user@ip-172-31-31-108.eu-west-1.compute.internal>
2018-05-10 14:28:59 +0000

    Added data.json loading

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-var express = require("express")
+var fs = require("fs");
+
+var express = require("express");
 var app = express();
 
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
+    var jsondata = fs.readFileSync('data.json');
+
+    var data = JSON.parse(jsondata);
+
     res.render("home");
-    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is listening...!");
